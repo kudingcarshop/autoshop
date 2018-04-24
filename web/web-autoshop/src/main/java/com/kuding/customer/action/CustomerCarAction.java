684f1981c29fd6ef11bf9130aaff2500c2408909
steven <kudingcar@163.com>
2018-04-24 21:05:06 +0800

    customer car detail implement

--- web/web-autoshop/src/main/java/com/kuding/customer/action/CustomerCarAction.java
+++ web/web-autoshop/src/main/java/com/kuding/customer/action/CustomerCarAction.java
@@ -1,7 +1,11 @@
 package com.kuding.customer.action;
 
+
+import java.sql.Date;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,10 +14,20 @@ import com.kuding.commons.BusinessException;
 import com.kuding.commons.ErrorCode;
 import com.kuding.commons.login.UserInfo;
 import com.kuding.garage.action.BasicAction;
+import com.kuding.garage.model.VehicleEntity;
+import com.kuding.garage.service.CustomerService;
+import com.kuding.garage.service.VehicleService;
+import com.kuding.util.Utils;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerCarAction extends BasicAction {
+	
+	@Autowired
+	private CustomerService custService;
+	
+	@Autowired
+	private VehicleService vehService;
 	
 	@RequestMapping("cars/add")
 	public ModelAndView add(HttpServletRequest req) {
@@ -37,13 +51,60 @@ public class CustomerCarAction extends BasicAction {
 		return mv;
 	}
 	
+	/**
+	 * 车辆详情
+	 * @param req
+	 * @param vehicleId
+	 * @return
+	 */
 	@RequestMapping("cars/detail")
-	public ModelAndView detail(HttpServletRequest req) {
+	public ModelAndView detail(HttpServletRequest req,Integer vehicleId) {
 		UserInfo user = getUserInfo(req.getSession());
-		if(user == null || user.getUserId() == null) {
+		if(user == null || user.getUserId() == null || vehicleId == null) {
 			throw new BusinessException(ErrorCode.SYS_ERROR);
 		}
 		ModelAndView mv = new ModelAndView();
+		//违章
+		long vioCount = custService.queryTrafficViolationCount(user.getUserId());
+		mv.getModel().put("vioCount", vioCount);
+		
+		//进度
+		long servCount = custService.queryUserServingCount(user.getUserId());
+		mv.getModel().put("servCount", servCount);
+		
+		VehicleEntity veh = vehService.findById(VehicleEntity.class, vehicleId);
+		if(veh != null) { 
+			if(veh.getRegisterDate() != null) {
+				//保险到期日
+				Date insuranceDate = Utils.assumeInsuranceDate(veh.getRegisterDate());
+				if(insuranceDate != null) {
+					mv.getModel().put("insuranceDate", insuranceDate);
+				}
+				//保险到期提醒标志
+				mv.getModel().put("insuranceInform", Utils.isNeedInsurance(veh.getRegisterDate()));
+				
+				//年审
+				Date vertificationDate = Utils.assumeVertificationDate(veh.getRegisterDate());
+				if(vertificationDate != null) {
+					mv.getModel().put("vertificationDate", vertificationDate);
+				}
+				//年审提醒标志
+				mv.getModel().put("vertificationInform", Utils.isNeedAnnualVertification(veh.getRegisterDate()));
+			}
+			
+			//保养
+			if(veh.getLastMaintainDate() != null) {
+				//保养到期日
+				Date maintainDate = Utils.assumeMaintaineDate(veh.getLastMaintainDate());
+				if(maintainDate != null) {
+					mv.getModel().put("maintainDate", maintainDate);
+				}
+				//年审提醒标志
+				mv.getModel().put("maintainInform", Utils.isNeedMaintaine(veh.getLastMaintainDate()));
+			}
+			
+		}
+		
 		mv.setViewName("customer/cars/car_detail");
 		return mv;
 	}
--- web/web-autoshop/src/main/java/com/kuding/util/Utils.java
+++ web/web-autoshop/src/main/java/com/kuding/util/Utils.java
@@ -49,7 +49,38 @@ public class Utils {
 	}
 	
 	/**
-	 * 根据车辆注册日期判断是否需要提醒保险
+	 * 推算车辆年间日期
+	 * @param date
+	 * @return
+	 */
+	public static Date assumeVertificationDate(Date date) {
+		if(date != null) {
+			Calendar regCal = Calendar.getInstance();
+			regCal.setTime(date);
+			
+			//年检截止日期
+			Calendar deadline = Calendar.getInstance();
+			deadline.set(Calendar.MONTH, regCal.get(Calendar.MONTH));
+			deadline.set(Calendar.DAY_OF_MONTH,regCal.get(Calendar.DAY_OF_MONTH));
+			
+			//车辆购买时常
+			long days = (deadline.getTime().getTime()-regCal.getTime().getTime())/1000/60/60/24;
+			//六年内新车
+			if(days/365<=6) {
+				//不到两年间隔，则下一年年检
+				if(days/365%2>0) {
+					deadline.add(Calendar.YEAR, 1);
+				}
+			}
+			
+			return new Date(deadline.getTimeInMillis());
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * 推测是否需要提醒保险到期
 	 * @param date
 	 * @return
 	 */
@@ -73,6 +104,26 @@ public class Utils {
 		}
 		return false;
 	}
+	
+	/**
+	 * 推算车辆保险日期
+	 * @param date
+	 * @return
+	 */
+	public static Date assumeInsuranceDate(Date date) {
+		if(date != null) {
+			Calendar regCal = Calendar.getInstance();
+			regCal.setTime(date);
+
+			//保险到期日
+			Calendar deadline = Calendar.getInstance();
+			deadline.set(Calendar.MONTH, regCal.get(Calendar.MONTH));
+			deadline.set(Calendar.DAY_OF_MONTH,regCal.get(Calendar.DAY_OF_MONTH));
+			return new Date(deadline.getTimeInMillis());
+		}
+		return null;
+	}
+
 	
 	/**按半年一次保养进行提醒
 	 * 判断是否需要提醒进行保养
@@ -107,6 +158,53 @@ public class Utils {
 			
 		}
 		return false;
+	}
+	
+	/**
+	 * 推算下一个保养日期
+	 * @param date
+	 * @return
+	 */
+	public static Date assumeMaintaineDate(Date date) {
+		if(date != null) {
+			Calendar regCal = Calendar.getInstance();
+			regCal.setTime(date);
+			
+			//当前
+			Calendar curcal = Calendar.getInstance();
+
+			//到期日
+			Calendar deadline = Calendar.getInstance();
+			deadline.set(Calendar.MONTH, regCal.get(Calendar.MONTH));
+			deadline.set(Calendar.DAY_OF_MONTH,regCal.get(Calendar.DAY_OF_MONTH));
+			
+			//到期日之前
+			Calendar preCal = Calendar.getInstance();
+			preCal.set(Calendar.MONTH, regCal.get(Calendar.MONTH));
+			preCal.set(Calendar.DAY_OF_MONTH,regCal.get(Calendar.DAY_OF_MONTH));
+			preCal.add(Calendar.MONTH, -6);
+			long diff = preCal.getTimeInMillis()-curcal.getTimeInMillis();
+			if(diff>=0) {
+				return new Date(preCal.getTimeInMillis());
+			}
+			
+			diff = deadline.getTimeInMillis() - curcal.getTimeInMillis();
+			if(diff>=0) {
+				return new Date(deadline.getTimeInMillis());
+			}
+			
+			//到期日之后
+			Calendar afterCal = Calendar.getInstance();
+			afterCal.set(Calendar.MONTH, regCal.get(Calendar.MONTH));
+			afterCal.set(Calendar.DAY_OF_MONTH,regCal.get(Calendar.DAY_OF_MONTH));
+			afterCal.add(Calendar.MONTH, 6);
+			diff = afterCal.getTimeInMillis() - curcal.getTimeInMillis();
+			if(diff>=0) {
+				return new Date(afterCal.getTimeInMillis());
+			}
+			
+		}
+		return null;
 	}
 
 }
